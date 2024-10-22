e539451e56f5d08d716384375930c2ac56432f09
caperaven <caperaven@gmail.com>
2024-10-22 19:12:55 +0200

    ollama add infrastructure

--- components/ollama-ui/ollama-ui.js
+++ components/ollama-ui/ollama-ui.js
@@ -1,5 +1,12 @@
 import {ComponentModule} from '../../src/modules/component.js';
 
+const TRANSLATION_MAP = {
+    "placeholder": "input:placeholder",
+    "run-hint": "#btnRun:title",
+    "attach-hint": "#btnAttach:title",
+    "settings-hint": "#btnSettings:title"
+}
+
 export class OllamaUIComponent extends HTMLElement {
     static name = Object.freeze('ollama-ui');
 
@@ -14,6 +21,25 @@ export class OllamaUIComponent extends HTMLElement {
 
     async disconnectedCallback() {
 
+    }
+
+    /**
+     * Sets the translations for the component
+     * {
+     *     "placeholder"    : "Enter your text here",
+     *     "run-hint"       : "Run",
+     *     "attach-hint"    : "Attach a file",
+     *     "settings-hint"  : "Settings"
+     * }
+     * @param dictionary
+     */
+    setTranslations(dictionary) {
+        for (const key of Object.keys(dictionary)) {
+            if (TRANSLATION_MAP[key]) {
+                const parts = TRANSLATION_MAP[key].split(':');
+                this.shadowRoot.querySelector(`${parts[0]}`).setAttribute(parts[1], dictionary[key]);
+            }
+        }
     }
 }
 
