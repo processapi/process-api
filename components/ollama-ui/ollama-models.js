93e22ae483fa65c2d62cc7f25c0f3265a0146785
caperaven <caperaven@gmail.com>
2024-10-28 18:54:33 +0200

    ollama add filter features

--- components/ollama-ui/ollama-models.js
+++ components/ollama-ui/ollama-models.js
@@ -1,6 +1,16 @@
 import { ComponentModule } from "../../src/modules/component.js";
 import { OllamaModule } from "../../src/modules/ollama.js";
 
+/**
+ * @class OllamaModels
+ * @description Custom element for displaying Ollama models.
+ * It displays the models available for installation.
+ * It also displays if the model supports tooling and additional information about the model.
+ * You can install models and delete models.
+ *
+ * @example
+ * <ollama-models></ollama-models>
+ */
 export class OllamaModels extends HTMLElement {
     static name = Object.freeze("ollama-models");
     #models;
@@ -14,6 +24,11 @@ export class OllamaModels extends HTMLElement {
         this.attachShadow({ mode: "open" });
     }
 
+    /**
+     * @method connectedCallback
+     * @description Lifecycle method called when the element is added to the DOM.
+     * @returns {Promise<void>}
+     */
     async connectedCallback() {
         this.shadowRoot.innerHTML = await ComponentModule.load_html({
             url: import.meta.url,
@@ -36,6 +51,11 @@ export class OllamaModels extends HTMLElement {
         this.shadowRoot.querySelector("[type='search']").addEventListener("input", this.#filterChangeHandler);
     }
 
+    /**
+     * @method disconnectedCallback
+     * @description Lifecycle method called when the element is removed from the DOM.
+     * @returns {Promise<void>}
+     */
     async disconnectedCallback() {
         this.shadowRoot.querySelector(".available").removeEventListener("click", this.#selectedHandler);
         this.shadowRoot.querySelector("[type='search']").addEventListener("input", this.#filterChangeHandler);
@@ -45,6 +65,12 @@ export class OllamaModels extends HTMLElement {
         this.#models = null;
     }
 
+    /**
+     * @method #selectFirstModel
+     * @description Select the first model in the list.
+     * This will display the first model in the list as the selected model details.
+     * @returns {void}
+     */
     #selectFirstModel() {
         const parentElement = this.shadowRoot.querySelector(".model");
         const firstModelName = Object.keys(this.#models)[0];
@@ -54,6 +80,13 @@ export class OllamaModels extends HTMLElement {
         updateSelected(this.shadowRoot, parentElement, model);
     }
 
+    /**
+     * @method #selected
+     * @description Event handler for when a model is selected.
+     * When a model is selected, display the model details in the selected model section.
+     * @param event
+     * @returns {Promise<void>}
+     */
     async #selected(event) {
         const target = event.composedPath()[0];
         const modelName = target.querySelector(".model-name").textContent.trim();
@@ -64,6 +97,12 @@ export class OllamaModels extends HTMLElement {
         updateSelected(this.shadowRoot, parentElement, model);
     }
 
+    /**
+     * @method #filterChanged
+     * @description Event handler for when the filter input changes.
+     * @param event
+     * @returns {Promise<void>}
+     */
     async #filterChanged(event) {
         const target = event.composedPath()[0];
         const text = target.value.trim().toLowerCase();
@@ -71,6 +110,15 @@ export class OllamaModels extends HTMLElement {
     }
 }
 
+/**
+ * @function buildListItems
+ * @description Build the list items for the models.
+ * Display the model name and the size of the model.
+ * If the model is installed, display the installed status.
+ * @param shadowRoot - Shadow root of the custom element
+ * @param models - Models to display
+ * @param installed - Currently installed models
+ */
 function buildListItems(shadowRoot, models, installed) {
     const template = shadowRoot.querySelector("#models-template");
     const parentElement = shadowRoot.querySelector(".available");
@@ -89,6 +137,14 @@ function buildListItems(shadowRoot, models, installed) {
     }
 }
 
+/**
+ * @function updateSelected
+ * @description Update the selected model details.
+ * Display the model name, URL, tools, and sizes of the model.
+ * @param shadowRoot - Shadow root of the custom element
+ * @param parentElement - Parent element to display the model details
+ * @param item - Model details to display
+ */
 function updateSelected(shadowRoot, parentElement, item) {
     parentElement.querySelector(".model-name").textContent = item.name;
 
@@ -104,6 +160,14 @@ function updateSelected(shadowRoot, parentElement, item) {
     createSizes(sizesElement, shadowRoot, item.sizes);
 }
 
+/**
+ * @function createSizes
+ * @description Create the sizes for the model.
+ * Display the size name and the size value.
+ * @param parentElement - Parent element to display the sizes
+ * @param shadowRoot - Shadow root of the custom element
+ * @param sizes - Sizes to display
+ */
 function createSizes(parentElement, shadowRoot, sizes) {
     const template = shadowRoot.querySelector("#sizes-template");
     parentElement.innerHTML = "";
