1c03cc5e24d5f2afba4134cf568b23c4708d1c99
caperaven <caperaven@gmail.com>
2024-11-10 11:57:40 +0200

    add markdown procesor to hight memo

--- components/highlight-memo/highlight-memo.js
+++ components/highlight-memo/highlight-memo.js
@@ -1,4 +1,5 @@
 import {ComponentModule} from "../../src/modules/component.js";
+import {MarkdownModule} from "../../src/modules/markdown.js";
 
 export class HighlightMemo extends HTMLElement {
     static name = Object.freeze("highlight-memo");
@@ -17,14 +18,28 @@ export class HighlightMemo extends HTMLElement {
     }
 
     async disconnectedCallback() {
+        this.#row = null;
     }
 
     push(word) {
-        if (word.trim().length === 0) {
+        return new Promise(async resolve => {
+            if (word.trim().length === 0) {
+                const html = await MarkdownModule.to_html({
+                    markdown: this.#row.join(""),
+                });
 
-        }
+                const template = document.createElement("template");
+                template.innerHTML = html;
+                this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-        this.#row.push(word);
+                this.#row = [];
+                this.scrollTop = this.scrollHeight;
+                resolve();
+            }
+
+            this.#row.push(word);
+            resolve();
+        })
     }
 }
 
