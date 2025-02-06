55f8d29cc65b2db475969ab70e979f3840422b7d
caperaven <caperaven@gmail.com>
2025-02-06 23:34:38 +0200

    polish toast notification

--- app/form/form.js
+++ app/form/form.js
@@ -1,6 +1,8 @@
-
+import "./../../components/toast-notification/toast-notification.js";
 export default class FormView extends HTMLElement {
 	static tag = "form-view";
+
+	#clickHandler = this.#click.bind(this);
 
 	constructor() {
 		super();
@@ -11,10 +13,33 @@ export default class FormView extends HTMLElement {
 		this.shadowRoot.innerHTML = await api.call("component", "load_html", {
 			url: import.meta.url,
 		});
+
+		this.shadowRoot.addEventListener("click", this.#clickHandler);
+	}
+
+	async disconnectedCallback() {
+		this.shadowRoot.removeEventListener("click", this.#clickHandler);
+		this.#clickHandler = null;
 	}
 
     load(data) {
     }
+
+	async #click(event) {
+		const target = event.target;
+		
+		if (target.dataset.action) {
+			this[target.dataset.action]();
+		}
+	}
+
+	error() {
+		toastNotification.error("Something bad happened");
+	}
+
+	info() {
+		toastNotification.info("Something interesting happened");
+	}
 }
 
 customElements.define(FormView.tag, FormView);
--- components/toast-notification/toast-notification.js
+++ components/toast-notification/toast-notification.js
@@ -1,7 +1,11 @@
 import {ComponentModule} from "../../src/modules/component.js";
 
 export class ToastNotification extends HTMLElement {
-    static name = Object.freeze("highlight-memo");
+    static name = Object.freeze("toast-notification");
+
+    get duration() {
+        return parseInt(this.getAttribute("duration")) || 3000;
+    }
 
     constructor() {
         super();
@@ -13,6 +17,60 @@ export class ToastNotification extends HTMLElement {
             url: import.meta.url,
         });
     }
+
+    #showTemplate(id, message) {
+        const template = this.shadowRoot.querySelector(`#${id}`);
+        const clone = template.content.cloneNode(true);
+        clone.querySelector("p").innerText = message;
+
+        const container = this.shadowRoot.querySelector(".toast-container");
+        const element = clone.firstElementChild;
+        container.appendChild(element);
+
+        setTimeout(() => {
+            element.remove();
+        }, this.duration);
+    }
+
+    info(message) {
+        this.#showTemplate("info-toast", message);
+    }
+
+    success(message) {
+        this.#showTemplate("success-toast", message);
+    }
+
+    warning(message) {
+        this.#showTemplate("warning-toast", message);
+    }
+
+    error(message) {
+        this.#showTemplate("error-toast", message);
+    }
 }
 
 customElements.define(ToastNotification.name, ToastNotification);
+
+globalThis.toastNotification = class {
+    static info(message) {
+        const toast = document.querySelector("toast-notification");
+        toast.info(message);
+    }
+
+    static success(message) {
+        const toast = document.querySelector("toast-notification");
+        toast.success(message);
+    }
+
+    static warning(message) {
+        const toast = document.querySelector("toast-notification");
+        toast.warning(message);
+    }
+
+    static error(message) {
+        const toast = document.querySelector("toast-notification");
+        toast.error(message);
+    }
+}
+
+document.body.appendChild(document.createElement("toast-notification"));
