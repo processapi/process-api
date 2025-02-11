ef447731213fbf03b646147c721059885fcde3de
caperaven <caperaven@gmail.com>
2025-02-11 21:47:24 +0200

    add chevron to group items

--- components/menu/menu-group/menu-group.js
+++ components/menu/menu-group/menu-group.js
@@ -1,4 +1,5 @@
 import { ComponentModule } from "../../../src/modules/component.js";
+import "./../../material-icon/material-icon.js";
 
 class MenuGroup extends HTMLElement {
     static name = Object.freeze("menu-group");
@@ -14,7 +15,21 @@ class MenuGroup extends HTMLElement {
             url: import.meta.url,
         });
 
+        this.#addChevron();
+
         this.style.display = "flex";
+    }
+
+    #addChevron() {
+        const chevron = document.createElement("material-icon");
+        chevron.setAttribute("icon", "chevron_right");
+        chevron.style.marginLeft = "auto";
+        chevron.setAttribute("slot", "suffix");
+        
+        requestAnimationFrame(() => {
+            const menuItem = this.querySelector("menu-item");
+            menuItem.appendChild(chevron);
+        })
     }
 }
 
