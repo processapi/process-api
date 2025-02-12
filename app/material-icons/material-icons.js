cbe311a59b0334d94fc8ec8a55a79b40dcbea241
caperaven <caperaven@gmail.com>
2025-02-12 17:37:59 +0200

    feat: implement dynamic icon search and layout for Material Icons component

--- app/material-icons/material-icons.js
+++ app/material-icons/material-icons.js
@@ -1,8 +1,12 @@
 import {ComponentModule} from "../../src/modules/component.js";
 import "./../../components/material-icon/material-icon.js";
+import {icons} from "./icons.js";
+import { EventsManager } from "../../src/system/events-manager.js";
 
 export default class MaterialIconsView extends HTMLElement {
 	static tag = "material-icons-view";
+
+	#eventsManager = new EventsManager();
 
 	constructor() {
 		super();
@@ -13,9 +17,48 @@ export default class MaterialIconsView extends HTMLElement {
 		this.shadowRoot.innerHTML = await ComponentModule.load_html({
 			url: import.meta.url,
 		});
+
+		this.#createIcons();
+		this.#setupSearch();
 	}
 
-	load(data) {
+	dispose() {
+		this.#eventsManager = this.#eventsManager.dispose();
+	}
+
+	#createIcons() {
+		const template = this.shadowRoot.querySelector("template");
+
+		const fragment = document.createDocumentFragment();
+		for (const icon of icons) {
+			const instance = template.content.cloneNode(true);
+
+			const materialIcon = instance.querySelector("material-icon");
+			materialIcon.setAttribute("icon", icon);
+
+			instance.firstElementChild.title = icon;
+
+			fragment.appendChild(instance);
+		}
+		this.shadowRoot.querySelector(".icons").appendChild(fragment);
+	}
+
+	#setupSearch() {
+		const searchInput = this.shadowRoot.querySelector(`[type="search"]`);
+		this.#eventsManager.addEvent(searchInput, "input", this.#search.bind(this));
+	}
+
+	#search(event) {
+		const search = event.target.value.toLowerCase();
+		const icons = this.shadowRoot.querySelectorAll("material-icon");
+		for (const icon of icons) {
+			const iconText = icon.getAttribute("icon").toLowerCase();
+			if (iconText.includes(search)) {
+				icon.parentElement.style.display = "block";
+			} else {
+				icon.parentElement.style.display = "none";
+			}
+		}
 	}
 }
 
