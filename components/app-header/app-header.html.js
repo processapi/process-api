0364c89d66e2a55d953a072b6bd3a27c95317c24
caperaven <caperaven@gmail.com>
2025-03-04 18:17:51 +0200

    feat: refactor app header button styles and update CSS imports for improved icon handling

--- components/app-header/app-header.html.js
+++ components/app-header/app-header.html.js
@@ -1,12 +1,12 @@
 
 export const HTML = `
-<link rel="stylesheet" href="__css__">
+<link rel="stylesheet" href="${new URL("./app-header.css", import.meta.url)}">
 <slot name="prefix"></slot>
 
-<button class="icon-button" data-action="home">
+<button class="icon" data-action="home">
     <material-icon icon="home"></material-icon>
 </button>
 
 <slot></slot>
 <slot name="suffix"></slot>
-`.replace("__css__", import.meta.url.replace(".html.js", ".css"));
+`;
